2a7a56f9e792a7328cdbe3ac82928ca02bcd87ed
marnix <mvsoom@gmail.com>
2024-12-19 13:01:28 +0100

    Ignore posts if not published

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,20 +2,28 @@
 import { glob } from "astro/loaders";
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
+
 // Define a `loader` and `schema` for each collection
 const vault = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/vault" }),
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-      tags: z.array(z.string())
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/vault" }),
+  schema: z
+    .object({
+      title: z.string().optional(),
+      publish: z.boolean().default(false),
+      date: z.date().optional(),
+      tags: z.array(z.string()).default([]),
+      image: z
+        .object({
+          url: z.string(),
+          alt: z.string(),
+        })
+        .optional(),
     })
+    .refine((data) => !data.publish || (data.publish && data.title && data.date), {
+      message: "Set `title` and `date` if `publish: true`",
+      path: ["title", "date"], // Path to the error
+    }),
 });
+
 // Export a single `collections` object to register your collection(s)
 export const collections = { vault };
--- src/vault/post-1.md
+++ src/vault/post-1.md
@@ -1,10 +1,10 @@
 ---
+publish: true
 title: 'My First Blog Post'
-pubDate: 2022-07-01
-description: 'This is the first post of my new Astro blog.'
-author: 'Astro Learner'
+date: 2022-07-01
 image:
     url: 'https://docs.astro.build/assets/rose.webp'
     alt: 'The Astro logo on a dark background with a pink glow.'
-tags: ["astro", "blogging", "learning in public"]
 ---
+
+Post 1
