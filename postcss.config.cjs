eb6114110885883962998b571a5db29c77aef230
marnix <mvsoom@gmail.com>
2024-12-28 11:59:44 +0100

    Enable custom media queries

---
+++ postcss.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    plugins: [
+        /* Without any configuration options, PostCSS Preset Env enables Stage 2 features and supports all browsers */
+        require('postcss-preset-env'),
+    ],
+};
