c48a8a17d40c5f14316ff5f6bd87c2a2e93d26b0
marnix <mvsoom@gmail.com>
2024-12-29 18:32:26 +0100

    Make footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@
 import { defineConfig } from 'astro/config';
 import rehypeAstroRelativeMarkdownLinks from "astro-rehype-relative-markdown-links";
 
-const options = {
-};
-
-// https://github.com/vernak2539/astro-rehype-relative-markdown-links
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
   markdown: {
-    rehypePlugins: [[rehypeAstroRelativeMarkdownLinks, options]],
+    rehypePlugins: [rehypeAstroRelativeMarkdownLinks],
+  },
+  experimental: {
+    svg: true,
   },
 });
