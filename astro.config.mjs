31dd38e13b83dcd400aac5a172dc54b736321101
marnix <mvsoom@gmail.com>
2025-01-05 16:50:41 +0100

    Make markdown links relative to root /, not /vault

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import rehypeAstroRelativeMarkdownLinks from "astro-rehype-relative-markdown-links";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
   markdown: {
-    rehypePlugins: [rehypeAstroRelativeMarkdownLinks],
+    rehypePlugins: [
+      [
+        rehypeAstroRelativeMarkdownLinks,
+        {
+          /* Set the base path to / such that the Markdown content in src/content/vault all just map to mysite.com/[...slug] instead of mysite.com/vault/[...slug] */
+          collectionBase: false,
+        },
+      ],
+    ],
   },
   experimental: {
     svg: true,
