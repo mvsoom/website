101c9cd2d032a20065cace927a2ac9690a86568a
marnix <mvsoom@gmail.com>
2025-01-09 18:38:13 +0100

    Simpler justified SiteNavigation

--- src/components/PaginatedPosts/media.js
+++ src/components/PaginatedPosts/media.js
@@ -3,7 +3,7 @@
 // ... but here it works perfectly well and it is processed by vite as expected
 import InfiniteScroll from 'infinite-scroll';
 import Masonry from 'masonry-layout';
-import ImagesLoaded from 'imagesloaded';
+import imagesLoaded from 'imagesloaded';
 
 const dataset = document.querySelector("#data").dataset;
 const tag = dataset.tag;
@@ -18,17 +18,59 @@ function getNextYearPath() {
   }
 }
 
-// init Masonry
-let msnry = new Masonry(container, {});
+/* 
 
-// make imagesLoaded available for InfiniteScroll
-InfiniteScroll.imagesLoaded = ImagesLoaded;
+Images sometimes stacked on top of eachother
+Refresh helps but not always
+This issue:
 
-const infScroll = new InfiniteScroll(container, {
+https://github.com/metafizzy/infinite-scroll/issues/978
+
+SOLVED by using <Image>s instead of <Picture>s !!! ???
+
+*/
+
+let grid = document.querySelector(container);
+
+
+let msnry = new Masonry( grid, {
+  itemSelector: 'none', // select none at first
+  stagger: 30,
+});
+
+console.log("data:", document.querySelector("#data"));
+
+
+// initial items reveal
+imagesLoaded( grid, function() {
+  console.log('imagesLoaded');
+  
+  console.log(grid);
+
+  grid.classList.remove('are-images-unloaded');
+
+
+  console.log(grid);
+
+  msnry.options.itemSelector = 'li';
+  let items = grid.querySelectorAll('li');
+  console.log("items: ", items);
+
+  msnry.appended( items );
+});
+
+InfiniteScroll.imagesLoaded = imagesLoaded;
+
+//-------------------------------------//
+// init Infinte Scroll
+
+let infScroll = new InfiniteScroll( grid, {
   path: getNextYearPath,
+  
   append: target + " ol",
   prefill: true,
   history: false,
   checkLastPage: true,
   outlayer: msnry,
+  debug: true,
 });
