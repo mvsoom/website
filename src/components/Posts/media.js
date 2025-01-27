b834133f32fe9a3d0c087a239afb1e7580df3b6c
marnix <mvsoom@gmail.com>
2025-01-27 01:11:05 +0100

    Implement working resizeable Masonry prototype without imgs

--- src/components/Posts/media.js
+++ src/components/Posts/media.js
@@ -5,5 +5,56 @@ const dataset = document.querySelector("#data").dataset;
 
 const target = dataset.target;
 
-var msnry = new Masonry(target, {
+const grid = document.querySelector(target);
+const msnry = new Masonry(grid, {
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
 });
+
+grid.addEventListener('click', (event) => {
+    if (!event?.target?.matches('.grid-item-content')) return;
+    const itemContent = event.target;
+    setItemContentPixelSize(itemContent);
+
+    const itemElem = itemContent.parentNode;
+    itemElem.classList.toggle('is-expanded', !itemElem.classList.contains('is-expanded'));
+
+    itemContent.offsetWidth; // force redraw
+    itemContent.style[transitionProp] = '';
+
+    addTransitionListener(itemContent);
+    setItemContentTransitionSize(itemContent, itemElem);
+
+    msnry.layout();
+});
+
+const docElem = document.documentElement;
+const transitionProp = typeof docElem.style.transition === 'string' ?
+    'transition' : 'WebkitTransition';
+const transitionEndEvent = {
+    WebkitTransition: 'webkitTransitionEnd',
+    transition: 'transitionend'
+}[transitionProp];
+
+function setItemContentPixelSize(itemContent) {
+    const { width, height } = itemContent.getBoundingClientRect();
+    itemContent.style[transitionProp] = 'none';
+    itemContent.style.width = `${width}px`;
+    itemContent.style.height = `${height}px`;
+}
+
+function addTransitionListener(itemContent) {
+    const onTransitionEnd = () => {
+        itemContent.style.width = '';
+        itemContent.style.height = '';
+        itemContent.removeEventListener(transitionEndEvent, onTransitionEnd, false);
+    };
+    itemContent.addEventListener(transitionEndEvent, onTransitionEnd, false);
+}
+
+function setItemContentTransitionSize(itemContent, itemElem) {
+    const { width, height } = itemElem.getBoundingClientRect();
+    itemContent.style.width = `${width}px`;
+    itemContent.style.height = `${height}px`;
+}
