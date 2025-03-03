1eea0f78966cf215855ebada34f74800032412db
marnix <mvsoom@gmail.com>
2025-03-03 18:34:04 +0100

    Unwrap images from p elements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import rehypeAstroRelativeMarkdownLinks from "astro-rehype-relative-markdown-links";
+import rehypeUnwrapImages from 'rehype-unwrap-images';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
           collectionBase: false,
         },
       ],
+      rehypeUnwrapImages,
     ],
   },
   experimental: {
